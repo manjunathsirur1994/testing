8c5de25f415006a8fd22aeb8bb3769a35ac4aadb
manjunath_sirur <manjunath.sirur@zapcg.com>
2022-07-17 19:25:03 +0530

    this is mongobd test push in git

---
+++ connection.py
@@ -0,0 +1,19 @@
+
+import pymongo
+
+
+client = pymongo.MongoClient("mongodb+srv://manjunathsirur:Jack_3140@test.y1bdo.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
+
+d = {
+    'name':'manju',
+    'email':'manjunathsirur1994@gmail.com',
+    'sir_name':'sirur'
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
